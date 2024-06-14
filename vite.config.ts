fa6e989159b28e1ed2998bfd100a42bd3f5e8697
Raku Zeta <zetaraku@gmail.com>
2024-06-15 00:05:57 +0800

    🔧 add src alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
+import * as path from 'node:path';
 import { defineConfig } from 'vite';
 import preact from '@preact/preset-vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [preact()],
+	resolve: {
+		alias: {
+			'@': path.resolve(__dirname, './src'),
+		},
+	},
 });
