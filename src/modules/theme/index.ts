7f303192d6003d2b234a9c0980d0ddceea8bc686
Raku Zeta <zetaraku@gmail.com>
2024-06-16 11:21:52 +0800

    🔥 remove redundant exports from modules

--- src/modules/theme/index.ts
+++ src/modules/theme/index.ts
@@ -1,4 +1,3 @@
 export { ThemeProvider } from './providers/ThemeProvider';
 export { ThemeChanger } from './components/ThemeChanger';
 export { useTheme } from './hooks/useTheme';
-export { availableThemes, type Theme } from './shared';
--- src/modules/user/index.ts
+++ src/modules/user/index.ts
@@ -1,3 +1,2 @@
 export { useUsers } from './hooks/useUsers';
 export { useUser } from './hooks/useUser';
-export { allUsers, type User } from './shared';
