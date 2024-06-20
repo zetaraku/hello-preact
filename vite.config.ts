f4135874900dae71b5d2f7738b69a76cb9c0c7e7
Raku Zeta <zetaraku@gmail.com>
2024-06-20 15:33:18 +0800

    🔧 setup in-source testing

--- vite.config.ts
+++ vite.config.ts
@@ -12,5 +12,11 @@ export default defineConfig({
 	},
 	test: {
 		environment: 'jsdom',
+		includeSource: [
+			'./src/**/*.{js,ts,jsx,tsx}',
+		],
+	},
+	define: {
+		'import.meta.vitest': 'undefined',
 	},
 });
