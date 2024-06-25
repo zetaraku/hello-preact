53347a2154e9ce4caf484b40b7d920b41ab5b7c0
Raku Zeta <zetaraku@gmail.com>
2024-06-25 22:06:45 +0800

    ✨ use real api for fake users ;)

--- src/modules/user/api/index.ts
+++ src/modules/user/api/index.ts
@@ -1,15 +1,40 @@
-import { allUsers, User } from '../shared';
+import { User } from '../shared';
+
+type RandomAPIUser = {
+	login: {
+		username: string,
+	},
+	name: {
+		first: string,
+	},
+};
+
+type RandomAPIData = {
+	results: RandomAPIUser[],
+};
 
 export async function getUsers(): Promise<User[]> {
-	await new Promise((resolve) => setTimeout(resolve, 500));
+	const data = await fetch('https://randomuser.me/api/?seed=hello-preact&results=5')
+		.then(async (response) => await response.json() as RandomAPIData);
 
-	return allUsers.slice();
+	const users = data.results.map((result) => ({
+		username: result.login.username,
+		name: result.name.first,
+	}));
+
+	return users;
 }
 
 export async function getUser(username: string): Promise<User> {
-	await new Promise((resolve) => setTimeout(resolve, 500));
+	const data = await fetch('https://randomuser.me/api/?seed=hello-preact&results=5')
+		.then(async (response) => await response.json() as RandomAPIData);
 
-	const user = allUsers.find((user) => user.username === username);
+	const users = data.results.map((result) => ({
+		username: result.login.username,
+		name: result.name.first,
+	}));
+
+	const user = users.find((user) => user.username === username);
 
 	if (user === undefined) {
 		throw new Error(`The user with username "${username}" doesn't exist.`);
--- src/modules/user/shared.ts
+++ src/modules/user/shared.ts
@@ -2,10 +2,3 @@ export type User = {
 	username: string,
 	name: string,
 };
-
-export const allUsers: User[] = [
-	{ username: 'alice', name: 'Alice' },
-	{ username: 'bob', name: 'Bob' },
-	{ username: 'cindy', name: 'Cindy' },
-	{ username: 'raku', name: 'Raku' },
-];
