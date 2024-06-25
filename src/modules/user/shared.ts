c85a05f9a4dfa724e89124e96b6699e5ad6e17db
Raku Zeta <zetaraku@gmail.com>
2024-06-25 19:30:14 +0800

    🩹 define User explicitly

--- src/modules/user/shared.ts
+++ src/modules/user/shared.ts
@@ -1,8 +1,11 @@
-export const allUsers = [
+export type User = {
+	username: string,
+	name: string,
+};
+
+export const allUsers: User[] = [
 	{ username: 'alice', name: 'Alice' },
 	{ username: 'bob', name: 'Bob' },
 	{ username: 'cindy', name: 'Cindy' },
 	{ username: 'raku', name: 'Raku' },
 ];
-
-export type User = typeof allUsers[number];
