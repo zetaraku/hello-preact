f65d80b3187274f562d1d8ff9c90defcac31b8db
Raku Zeta <zetaraku@gmail.com>
2024-07-12 21:15:00 +0800

    🩹 disable retry for ReactQuery

--- src/modules/app/providers/ReactQueryProvider.tsx
+++ src/modules/app/providers/ReactQueryProvider.tsx
@@ -5,6 +5,7 @@ import * as ReactQuery from '@tanstack/react-query';
 const queryClientConfig: ReactQuery.QueryClientConfig = {
 	defaultOptions: {
 		queries: {
+			retry: false,
 			refetchOnWindowFocus: false,
 		},
 	},
