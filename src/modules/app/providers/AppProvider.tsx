ad85003511c50640f964523deb75e090ef5d5b52
Raku Zeta <zetaraku@gmail.com>
2024-07-12 12:43:53 +0800

    ✨ use QueryClientProvider

--- src/modules/app/providers/AppProvider.tsx
+++ src/modules/app/providers/AppProvider.tsx
@@ -1,11 +1,26 @@
 import * as Preact from 'preact';
 import * as PreactIso from 'preact-iso';
+import * as PreactHooks from 'preact/hooks';
+import * as ReactQuery from '@tanstack/react-query';
 import { ThemeProvider } from '@/modules/theme';
 import { MultiProvider } from '@/modules/util';
 
+const queryClientConfig: ReactQuery.QueryClientConfig = {
+	defaultOptions: {
+		queries: {
+			refetchOnWindowFocus: false,
+		},
+	},
+};
+
 export const AppProvider: Preact.FunctionComponent = (props) => {
+	const [queryClient] = PreactHooks.useState(
+		() => new ReactQuery.QueryClient(queryClientConfig),
+	);
+
 	return (
 		<MultiProvider providers={[
+			<ReactQuery.QueryClientProvider client={queryClient} key={null} />,
 			<PreactIso.LocationProvider key={null} />,
 			<ThemeProvider key={null} />,
 		]}>
